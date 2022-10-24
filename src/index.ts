b2936af07dfd3d0f160c4ae0ab2616580b24f680
Anto-MSHK <anjiilop25@gmail.com>
2022-10-24 13:25:41 +0300

    121

--- src/index.ts
+++ src/index.ts
@@ -48,7 +48,7 @@ class Manager {
       fs.truncate('./server.log', 0, () => {})
 
       ManagerLogs.INFO('Server', managerMSG.STARTED)
-      console.log(path.resolve(__dirname, 'src'))
+      console.log(path.resolve(__filename))
       var files = fs.readdirSync(path.resolve(__dirname))
       console.log(files)
       await this.checkStateFile()
