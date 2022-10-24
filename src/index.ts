9f1643a35383bfddb0763928607813494d782acc
Anto-MSHK <anjiilop25@gmail.com>
2022-10-24 13:14:36 +0300

    5555

--- src/index.ts
+++ src/index.ts
@@ -49,6 +49,8 @@ class Manager {
 
       ManagerLogs.INFO('Server', managerMSG.STARTED)
       console.log(path.resolve(__dirname))
+      var files = fs.readdirSync(path.resolve(__dirname))
+      console.log(files)
       await this.checkStateFile()
       var startData = { dateLastStartServer: new Date().toLocaleString('en-US', { timeZone: 'Europe/Moscow' }) }
       await this.addDataToState(startData)
