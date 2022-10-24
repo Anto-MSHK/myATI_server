efdd68a8bf432e3005b660476f4356edb12e0d7c
Anto-MSHK <anjiilop25@gmail.com>
2022-10-24 15:39:10 +0300

    43433d

--- src/index.ts
+++ src/index.ts
@@ -49,7 +49,7 @@ class Manager {
 
       ManagerLogs.INFO('Server', managerMSG.STARTED)
       console.log(path.resolve(__dirname))
-      var files = fs.readdirSync(path.resolve(__dirname, '/files'))
+      // var files = fs.readdirSync(path.resolve(__dirname+ '/files'))
       fs.mkdir(__dirname + '/files/schedule', err => {
         if (err) throw err // не удалось создать папку
         fs.mkdir(__dirname + '/files/schedule/spo', err => {
