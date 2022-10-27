1e143089cc68f89ecac6984bc5e20831ee2b6571
Anto-MSHK <anjiilop25@gmail.com>
2022-10-27 17:25:52 +0300

    5686567567

--- src/services/FileService.ts
+++ src/services/FileService.ts
@@ -137,7 +137,7 @@ class FileService {
             file.on('finish', () => {
               file.close()
               count++
-              var files = fs.readdirSync(path.resolve('app/src/files/schedule/vpo'))
+              var files = fs.readdirSync('app/src/files/schedule/vpo')
               console.log(files)
               if (i_vpo + i_spo === count) {
                 resolve()
